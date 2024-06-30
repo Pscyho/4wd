c0b49f5b98326dc080b34257c9b7be7736c78078
GADIGEPPA <139635907+Pscyho@users.noreply.github.com>
2024-06-30 22:24:15 +0530

    Update main.ino

--- 2wd/src/main.ino
+++ 2wd/src/main.ino
@@ -1,20 +1,17 @@
 const int in1 = 4;
 const int in2 = 6;
-
 const int in3 = 5;
 const int in4 = 7;
 const int enA = 10;
 const int enB = 11;
-
 const int L_S = 13;
 const int R_S = 12;
 int M1_Speed = 110;           // speed of motor 1
 int M2_Speed = 110;           // speed of motor 2
 int Lspeed = 100;
-int Rspeed = 10;
+int Rspeed = 100;
 float kLeft = 0.1;
 float kRight = 0.1;
-
 #include <Arduino.h>
 void setup() {
     Serial.begin(9600);
@@ -22,18 +19,13 @@ void setup() {
     pinMode(in2, OUTPUT);
     pinMode(in3, OUTPUT);
     pinMode(in4, OUTPUT);
-
     pinMode(enA, OUTPUT);
     pinMode(enB, OUTPUT);
-
     analogWrite(enA, M1_Speed);
-    analogWrite(enB, M2_Speed);
-}
-
+    analogWrite(enB, M2_Speed);}
 void loop() {
     int LEFT_SENSOR = digitalRead(L_S);
     int RIGHT_SENSOR = digitalRead(R_S);
-
     int state;
 
     if (LEFT_SENSOR && RIGHT_SENSOR) {
@@ -47,10 +39,8 @@ void loop() {
         left();
     } else if (!LEFT_SENSOR && !RIGHT_SENSOR) {
         state = 3;  // go state
-        forward();
-    }
+        forward();  }
     int startTime = millis();
-
     switch (state) {
         case 0:
             analogWrite(enA, 0);
@@ -61,62 +51,43 @@ void loop() {
                 int RnewSpeed = Rspeed + kRight * (millis() - startTime);
                 analogWrite(enA, RnewSpeed);
                 int LnewSpeed = Lspeed - kLeft * (millis() - startTime);
-                analogWrite(enB, LnewSpeed);
-            }
+                analogWrite(enB, LnewSpeed);}
             break;
         case 2:
             while (digitalRead(R_S)) {
                 int RnewSpeed = Rspeed + kRight * (millis() - startTime);
                 analogWrite(enA, RnewSpeed);
                 int LnewSpeed = Lspeed - kLeft * (millis() - startTime);
-                analogWrite(enB, LnewSpeed);
-            }
+                analogWrite(enB, LnewSpeed);}
             break;
         case 3:  // go
-
             analogWrite(enA, Rspeed);
             analogWrite(enB, Lspeed);
-
             break;
         default: 
-
-            break;
-    }
-
-}
-
+            break;}}
 void forward() {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-}
+    digitalWrite(in4, LOW);}
 void backward() {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-}
-
+    digitalWrite(in4, HIGH);}
 void right() {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-}
-
+    digitalWrite(in4, LOW);}
 void left() {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
     digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-}
-
+    digitalWrite(in4, HIGH);}
 void Stop() {
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
     digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-}
+    digitalWrite(in4, LOW);}
