6a3f75f318c3a6fab6479ae83c40874fb7e5923d
nx2k3 <nx2k3@duck.com>
2024-07-05 16:26:05 +0530

    updated

--- src/main.ino
+++ src/main.ino
@@ -2,9 +2,9 @@
 #include <L298N.h>
 
 // IR Sensor pins
-const int L_IR = 0;  // left IR sensor pin
-const int R_IR = 0;  // right IR sensor pin
-const int M_IR = 0;  // mid IR sensor pin
+const int L_IR = 11;  // left IR sensor pin
+const int R_IR = 9;   // right IR sensor pin
+const int M_IR = 10;  // mid IR sensor pin
 
 // Buzzer pins
 const int BUZER = 0;
@@ -15,21 +15,21 @@ const int B_LED = 0;  // blue led pin
 const int G_LED = 0;  // green led pin
 
 // UV pins
-const int ECHO = 0;
-const int TRIG = 0;
+const int ECHO = 12;
+const int TRIG = 13;
 
 // MOTOR DRIVE
-const int EA = 0;
-const int EB = 0;
-const int IN1 = 0;
-const int IN2 = 0;
-const int IN3 = 0;
-const int IN4 = 0;
+const int EA = 3;
+const int EB = 5;
+const int IN1 = 8;
+const int IN2 = 7;
+const int IN3 = 4;
+const int IN4 = 2;
 
 L298N L_Motor(EA, IN1, IN2);
-L298N R_Motor(EB, IN3, IN4);
+L298N R_Motor(EB, IN4, IN3);
 
-const int MOTOR_SPEED = 50;
+const int MOTOR_SPEED = 70;
 
 // UV pins related variables
 long duration;
@@ -49,7 +49,130 @@ void get_distance() {
     Serial.print(distance);
     Serial.println(" cm");
 }
+void get_block() {
+}
 
+void move_bot() {
+    int LeftSensor = digitalRead(L_IR);
+    int MidVal = digitalRead(M_IR);
+    int RightVal = digitalRead(R_IR);
+
+    Serial.print("Left val:");
+    Serial.print(LeftSensor?"W":"B");
+
+    Serial.print(",Mid val:");
+    Serial.print(MidVal?"W":"B");
+
+    Serial.print(",Right val:");
+    Serial.println(RightVal?"W":"B");
+
+    //  if (LeftSensor == 0 && MidVal == 1 && RightVal == 0) {
+    //     L_Motor.forward();
+    //     R_Motor.forward();
+    //     Serial.println("forward");
+    // }
+    // else if (LeftSensor == 0 && MidVal == 1 && RightVal == 1) {
+    //     L_Motor.backward();
+    //     R_Motor.forward();
+    //     Serial.println("left");
+    // }
+    //     else if (LeftSensor == 0 && MidVal == 0 && RightVal == 1) {
+    //     L_Motor.backward();
+    //     R_Motor.forward();
+    //     Serial.println("left");
+    // }else if (LeftSensor == 1 && MidVal == 1 && RightVal == 0) {
+    //     R_Motor.backward();
+    //     L_Motor.forward();
+    //     Serial.println("right");
+    // }else if (LeftSensor == 0 && MidVal == 0 && RightVal == 0) {
+    //     L_Motor.stop();
+    //     R_Motor.stop();
+    //     Serial.println("stop");
+    // }else if (LeftSensor == 1 && MidVal == 1 && RightVal == 1) {
+    //     L_Motor.stop();
+    //     R_Motor.stop();
+    //     Serial.println("stop");
+    // }
+
+    if (LeftSensor == 1 && MidVal == 0 && RightVal == 1) {
+        L_Motor.forward();
+        R_Motor.forward();
+        Serial.println("forward");
+    } else if (LeftSensor == 0 && MidVal == 1 && RightVal == 0) {
+        L_Motor.forward();
+        R_Motor.forward();
+        Serial.println("forward");
+    }
+    
+    else if (LeftSensor == 0 && MidVal == 0 && RightVal == 0) {
+        L_Motor.stop();
+        R_Motor.stop();
+        Serial.println("stop");
+    } else if (LeftSensor == 1 && MidVal == 1 && RightVal == 1) {
+        L_Motor.stop();
+        R_Motor.stop();
+        Serial.println("stop");
+    } else if (LeftSensor == 0 && MidVal == 0 && RightVal == 1) {
+        L_Motor.backward();
+        R_Motor.forward();
+        //todo speed
+        Serial.println("slight left");
+    } else if (LeftSensor == 1 && MidVal == 0 && RightVal == 0) {
+        R_Motor.backward();
+        L_Motor.forward();
+                // todo speed
+        Serial.println("slight right");
+    } else if (LeftSensor == 0 && MidVal == 1 && RightVal == 1) {
+        L_Motor.backward();
+        R_Motor.forward();
+        Serial.println("left");
+    } else if (LeftSensor == 1 && MidVal == 1 && RightVal == 0) {
+        R_Motor.backward();
+        L_Motor.forward();
+
+        Serial.println("right");
+    }
+}
+
+enum CZone {
+    C1,
+    C2,
+    C3
+};
+
+enum Tables {
+    T1,
+    T2,
+    T3,
+    T4,
+    T5,
+    T6,
+    T7,
+    T8,
+    T9
+};
+
+enum LED {
+    R,
+    G,
+    B,
+};
+
+// void set_led(bool val,LED l) {}
+//     switch (l) {
+//         case R:
+//             digitalWrite(R_LED, val);
+//             break;
+//         case B:
+//             digitalWrite(B_LED, val);
+//             break;
+//         case G:
+//             digitalWrite(G_LED, val);
+//             break;
+//         default:
+//             break;
+//     }
+// }
 void setup() {
     // Used to display information
     Serial.begin(9600);
@@ -75,4 +198,10 @@ void setup() {
     R_Motor.setSpeed(MOTOR_SPEED);
 }
 void loop() {
+    delay(1000);
+    move_bot();
+    // get_distance();
+    // if (distance>20){
+
+    // }
 }
